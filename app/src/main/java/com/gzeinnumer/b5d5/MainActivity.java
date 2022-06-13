9e961dbbf9db0ed7d505b2fcaf36bcbea71b57d3
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-06-13 11:52:24 +0700

    aradasda

--- app/src/main/java/com/gzeinnumer/b5d5/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/b5d5/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
